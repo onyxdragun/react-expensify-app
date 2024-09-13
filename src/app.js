40c25bd5b68f8d15deda4485061ef9d98892d89c
onyxdragun <tyler@dynamicshark.com>
2024-09-13 16:24:31 -0700

    Removed comments from app.js and test data

--- src/app.js
+++ src/app.js
@@ -11,15 +11,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-// store.subscribe(() => {
-//   const state = store.getState();
-//   const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// });
-
-store.dispatch(addExpense({ description: 'Water bill', amount: '4500', createdAt: 5 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: '1500', createdAt: 10 }));
-store.dispatch(addExpense({ description: 'Rent', amount: '1950', createdAt: 1000 }));
-
 const root = ReactDOM.createRoot(document.querySelector('#app'));
 
 root.render(
