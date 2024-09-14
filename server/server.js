a581388c93ff81694830ec42a8396be653878eac
onyxdragun <tyler@dynamicshark.com>
2024-09-13 17:38:33 -0700

    Setup production build and server

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const port = process.env.POST || 3000;
+
+const app = express();
+const publicPath = path.join(__dirname, '..', 'public');
+
+app.use(express.static(publicPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log("Server is running...");
+});
