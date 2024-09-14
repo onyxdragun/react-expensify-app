2f54af44a38613799edff240e02e38da8aa826d1
onyxdragun <tyler@dynamicshark.com>
2024-09-13 20:18:02 -0700

    Corrected process.env.POST to .PORT

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const port = process.env.POST || 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
